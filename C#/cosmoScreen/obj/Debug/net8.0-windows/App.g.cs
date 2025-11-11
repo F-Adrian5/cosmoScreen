68d1b0e9ac2d9ee1e76d03d534b34f16ef7fddec
F-Adrian5 <fodor.adrian-2021@keri.mako.hu>
2025-11-11 08:21:34 +0100

    c#

--- C#/cosmoScreen/obj/Debug/net8.0-windows/App.g.cs
+++ C#/cosmoScreen/obj/Debug/net8.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace cosmoScreen {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.8.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace cosmoScreen {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.8.0")]
         public static void Main() {
             cosmoScreen.App app = new cosmoScreen.App();
             app.InitializeComponent();
--- C#/cosmoScreen/obj/Debug/net8.0-windows/cosmoScreen.AssemblyInfo.cs
+++ C#/cosmoScreen/obj/Debug/net8.0-windows/cosmoScreen.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cosmoScreen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1224c21d9d7277c0bb8b1ff6ce13d121db4f9c67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("cosmoScreen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cosmoScreen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
