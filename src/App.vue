a74bf86adbe641988b8638d1f2693faafa35841f
F-Adrian5 <fodor.adrian-2021@keri.mako.hu>
2025-11-13 14:06:43 +0100

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './views/Navbar.vue';
 </script>
 
 <template>
-  <header class="p-3 bg-light">
-    <div class="container d-flex flex-wrap justify-content-center">
-      <RouterLink to="/" class="nav-link px-2 link-secondary">Home</RouterLink>
-      <RouterLink to="/about" class="nav-link px-2 link-secondary">About</RouterLink>
-    </div>
+  <header>
+    <Navbar/>
   </header>
 
   <main class="container mt-5">
--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -1,5 +1,54 @@
 <template>
+  <nav class="navbar navbar-expand-sm bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" 
+         href="#">
+        CosmoScreen
+      </a>
+      
+      <!-- hamburger -->
+      <button class="navbar-toggler" 
+              type="button" 
+              data-bs-toggle="collapse" 
+              data-bs-target="#navbarNav" 
+              aria-controls="navbarNav" 
+              aria-expanded="false" 
+              aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      
+      <div class="collapse navbar-collapse" 
+           id="navbarNav">
 
-  
+        <!-- pages -->
+        <ul class="navbar-nav">
+          
+          <li class="nav-item">
+            <a class="nav-link active" 
+               aria-current="page"
+               href="#">Műsorlista</a>
+          </li>
 
+          <li class="nav-item">
+            <a class="nav-link" href="#">Büfé</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">Rólunk</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+
+          <span></span>
+          <li class="nav-item ms-3">Nyelv</li>
+          
+          <span></span>
+          <li class="nav-item ms-3">Profil</li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
