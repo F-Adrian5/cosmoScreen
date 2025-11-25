4b9e9719bc9e1479302ec5d1dc79f328797eff21
F-Adrian5 <fodor.adrian-2021@keri.mako.hu>
2025-11-25 11:35:39 +0100

    dokumentáció frissítése és treffló frissítése

--- src/components/CardCarousel.vue
+++ src/components/CardCarousel.vue
@@ -2,9 +2,8 @@
   <div class="container">
     <div class="row align-text-center">
       <div class="carousel w-100">
-        <div
-          class="card movie-card"
-          v-for="(movie, index) in movies" :key="movie.id" :style="{ '--i': index }">
+        <div class="card movie-card"
+             v-for="(movie, index) in movies" :key="movie.id" :style="{ '--i': index }">
           <img :src="movie.image" class="card-img-top" :alt="movie.title" />
           <div class="card-body">
             <h5 class="card-title">{{ movie.title }}</h5>
