38156f940ec2603fd9fd1d79f9e2ef3e53f09451
F-Adrian5 <fodor.adrian-2021@keri.mako.hu>
2025-11-27 10:32:54 +0100

    Büfé sablon

--- src/views/BuffetView.vue
+++ src/views/BuffetView.vue
@@ -1,5 +1,36 @@
 <template>
-  <div>
-    <h1>Buffet</h1>
+  <div class="container">
+    <div class="row">
+      <h1 class="text-center">Büfés ajánlataink</h1>
+      <hr>
+    </div>
+
+
+    <div class="d-flex mt-2">
+      <div>
+        <img src="https://dummyimage.com/250x200/000/fff">
+      </div>
+      <div>
+        <p class="ms-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum minus molestiae doloremque soluta vero optio. Cupiditate facere delectus nemo inventore quibusdam perspiciatis tempore reiciendis, repudiandae, culpa deserunt fuga veniam expedita!</p>
+      </div>
+    </div>
+
+    <div class="d-flex mt-2">
+      <div>
+        <img src="https://dummyimage.com/250x200/000/fff">
+      </div>
+      <div>
+        <p class="ms-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum minus molestiae doloremque soluta vero optio. Cupiditate facere delectus nemo inventore quibusdam perspiciatis tempore reiciendis, repudiandae, culpa deserunt fuga veniam expedita!</p>
+      </div>
+    </div>
+
+    <div class="d-flex mt-2">
+      <div>
+        <img src="https://dummyimage.com/250x200/000/fff">
+      </div>
+      <div>
+        <p class="ms-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum minus molestiae doloremque soluta vero optio. Cupiditate facere delectus nemo inventore quibusdam perspiciatis tempore reiciendis, repudiandae, culpa deserunt fuga veniam expedita!</p>
+      </div>
+    </div>
   </div>
 </template>
