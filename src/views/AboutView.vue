34485e15afe9f332f3aa443f6e31c873a9a36c16
F-Adrian5 <fodor.adrian-2021@keri.mako.hu>
2025-11-27 10:36:43 +0100

    Rólunk alapjának megcsinálása

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <div class="row">
+      <h1 class="text-center">Rólunk</h1>
+      <hr>
+      <h3>A csapat</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptatum consectetur molestias. Dolorum ab placeat rerum adipisci in et, iste officiis consectetur repellat sapiente distinctio alias natus libero obcaecati culpa?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptatum consectetur molestias. Dolorum ab placeat rerum adipisci in et, iste officiis consectetur repellat sapiente distinctio alias natus libero obcaecati culpa?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptatum consectetur molestias. Dolorum ab placeat rerum adipisci in et, iste officiis consectetur repellat sapiente distinctio alias natus libero obcaecati culpa?</p>
+    </div>
   </div>
 </template>
