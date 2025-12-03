79866cb48387badea7643de3c999ba0e992b74ec
F-Adrian5 <fodor.adrian-2021@keri.mako.hu>
2025-12-03 08:23:48 +0100

    backend változtatások + getMovieData.php

---
+++ backend/api/getMoviesData.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once("../../../common/php/environment.php");
+
+$db = new Database("cosmoscreen");
+
+$query = "SELECT `id`,`title`,`genre`,`runtime`,`director`,`production`,`age_restriction`,
+                 `showing_in`,`poster`,`trailer`,`description`,`release_date` 
+          FROM `movies`";
+
+$result = $db->execute($query);
+
+$db = null;
+
+Util::setResponse($result);
