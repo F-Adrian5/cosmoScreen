ff9af3f025ab02c442f7f6e0a18e8e0bb457b5da
F-Adrian5 <fodor.adrian-2021@keri.mako.hu>
2025-12-10 10:24:37 +0100

    műsorlista responzivitása és designja

--- src/views/ProgramListView.vue
+++ src/views/ProgramListView.vue
@@ -1,45 +1,26 @@
 <template>
-  <div class="container py-4">
-    <h1 class="text-center mb-2 mt-3">Műsorlista</h1>
-    <p class="text-center text-secondary fs-5 mb-4">
-      Ezeket a műsorokat vetítjük!
-    </p>
+  <div class="container my-3">
+    <div class="row g-3">
+      <div v-for="movie in movies" :key="movie.id"
+           class="movie-card p-2 h-100 px-2 py-2
+                  col-12 col-md-6 col-lg-4 col-xl-3">
 
-    <!-- Nap választó gombok -->
-    <div class="d-flex flex-wrap justify-content-center gap-2 mb-4">
-      <button v-for="day in days" :key="day" 
-              @click="selectDay(day)" 
-              :class="['btn', selectedDay === day ? 
-                        'btn-secondary' : 'btn-outline-secondary']">
-          {{ day }}
-      </button>
-    </div>
+        <div class="movie-content d-flex flex-row 
+                    flex-md-column border
+                    align-items-md-center border-black
+                    text-md-center">
+          <img :src="movie.image" class="movie-image">
 
-    <p class="text-center mb-4">
-      Kiválasztott nap: <strong>{{ selectedDay || 'nincs kiválasztva' }}</strong>
-    </p>
-
-    <!-- Filmek listája -->
-    <div class="container my-3">
-      <div v-for="movie in movies" :key="movie.id" 
-           class="card mb-3 movie-card">
-        <div class="row g-3 align-items-start">
-
-          <!-- Kép -->
-          <div class="col-auto">
-            <img :src="movie.image" 
-                 class="img-fluid rounded movie-image">
-          </div>
-        
-          <!-- Szöveg és időpontok -->
-          <div class="col-2">
-            <h3 class="mt-3 fs-5 mb-1">{{ movie.title }}</h3>
-            <p class="fs-6 mb-1 text-muted">{{ movie.genre }}</p>
-            <p class="fs-6 mb-1 text-muted">| {{ movie.length }}p</p>
+          <div class="mt-2 mx-2">
+            <h3 class="fs-3 mb-1 fw-bold">{{ movie.title }}</h3>
+            <p class="fs-5 mb-1 text-muted">{{ movie.genre }}</p>
+            <p class="fs-5 mb-3 text-muted">| {{ movie.length }} perc</p>
           
-            <!-- Időpontok -->
-            <div>
-              <span v-for="time in movie.times" :key="time" 
+            <div class="movie-times d-flex flex-wrap gap-1 
+                        justify-content-start 
+                        justify-content-md-center fs-5">
+
+              <span v-for="time in movie.times" :key="time"
                     class="badge bg-secondary me-1 mb-1">
                 {{ time }}
               </span>
