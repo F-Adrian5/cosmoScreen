0d2964799e2dc70402a9fdd5255058f00fb92db9
F-Adrian5 <fodor.adrian-2021@keri.mako.hu>
2026-01-08 11:00:46 +0100

    login frissitése de még nem jógit pull!

--- C#/cosmoScreen/login.xaml.cs
+++ C#/cosmoScreen/login.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,21 +21,60 @@ namespace cosmoScreen
     /// </summary>
     public partial class login : Window
     {
+        MySqlConnection connection = new MySqlConnection("server=localhost;database=cosmoscreen;uid=root");
+        MySqlCommand command;
+
         public login()
         {
             InitializeComponent();
         }
 
+        public void openConnection()
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        public void executeQuery(string query)
+        {
+            try
+            {
+                openConnection();
+
+                command = new MySqlCommand(query, connection);
+
+                if (command.ExecuteNonQuery() >= 1)
+                {
+                    MessageBox.Show($"Végrehajtva!");
+                }
+                else
+                {
+                    MessageBox.Show("Nem lett végrehajtva!");
+                }
+            }
+            catch (Exception hiba)
+            {
+                MessageBox.Show(hiba.Message);
+            }
+        }
+
         private void login_btn_Click(object sender, RoutedEventArgs e)
         {
-            if (loginE_input.Text == "a@a.aa" && loginP_input.Password == "1234Aa")
-            {
-               DialogResult = true;
-            }
-            else
-            {
-                MessageBox.Show("Hibás belépési adatok");
-            }
+            string email = loginE_input.Text;
+            string query = $"SELECT password,admin FROM users WHERE email = {email}";
+            executeQuery(query);
+            closeConnection();
+
         }
     }
 }
--- C#/cosmoScreen/obj/Debug/net9.0-windows7.0/App.g.cs
+++ C#/cosmoScreen/obj/Debug/net9.0-windows7.0/App.g.cs
@@ -47,7 +47,7 @@ namespace cosmoScreen {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.8.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -67,7 +67,7 @@ namespace cosmoScreen {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.8.0")]
         public static void Main() {
             cosmoScreen.App app = new cosmoScreen.App();
             app.InitializeComponent();
