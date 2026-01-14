07bc77606536cc448820942660d4dcbbb03bb305
KBalazs55 <krivan.balazs-2021@keri.mako.hu>
2026-01-14 08:26:01 +0100

    kommentelések

--- src/languages/language.ts
+++ src/languages/language.ts
@@ -2,29 +2,29 @@ import { reactive } from "vue";
 import hu from "./hu.json";
 import en from "./en.json";
 
-// define the language options
+//define the language options
 type Lang = "hu" | "en";
 
-// store the languages in an object
+//store the languages in an object
 const messages = { hu, en };
 
-// current language
+//current language
 const state = reactive<{ lang: Lang }>({
 
   //checks the localstorage for set language if there isnt one the default is hu
   lang: (localStorage.getItem("lang") as Lang) || "hu"
 });
 
-// translation function
-//key contains the key and value from the json file
+//translation function
+//key contains the key from the json file
 function t(key: string): string {
-  return key.split(".").reduce(
-    (obj: any, k) => obj?.[k],
-    messages[state.lang]
-  ) || key;
+  return key.split(".").reduce( //split and reduce the key
+    (obj: any, k) => obj?.[k], //go through the split key items
+    messages[state.lang] //the current language
+  ) || key; //if it cant find it it will give back the key
 }
 
-// language switch function
+//language switch function
 function setLang(lang: Lang) {
   state.lang = lang; //sets it to the chosen language
   localStorage.setItem("lang", lang); //save it into the local storage
--- src/vue-i18n.d.ts
+++ src/vue-i18n.d.ts
@@ -1,10 +1,10 @@
 import { ComponentCustomProperties } from 'vue';
 import language from '@/languages/language';
 
-declare module '@vue/runtime-core' { // needed to avoid errors
+declare module '@vue/runtime-core' { //needed to avoid errors
   interface ComponentCustomProperties {
-    $t: typeof language.t;
-    $setLang: typeof language.setLang;
-    $lang: typeof language.state;
+    $t: typeof language.t; //set the type of the translation function to the same as language.t
+    $setLang: typeof language.setLang; //set the type of the language switch function to the same as language.setLang  
+    $lang: typeof language.state; //set the type of $lang to the same as language.state
   }
 }
