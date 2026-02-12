7d7b97f309f2b2116ff91e57cf9089cf7a7fff4f
F-Adrian5 <fodor.adrian-2021@keri.mako.hu>
2026-02-12 09:44:20 +0100

    műsorlista redesign

--- src/views/ProgramList.vue
+++ src/views/ProgramList.vue
@@ -1,98 +1,115 @@
 <template>
-  <div class="container my-4">
+  <div class="container my-5">
     
-  <!-- dates -->
-  <div class="row justify-content-center">
-    <div class="col-12">
-      <h1 class="text-center text-white">
-        Vetítési szűrő
-      </h1>
-    
-      <hr class="text-white my-3">
-    </div>
-  
-    <!-- selection -->
-    <div class="row text-center fs-4">
-
-      <!-- day -->
-      <div class="w-50">       
-        <label for="daySelect" 
-             class="text-light">
-          Nap:
-        </label>
-
-        <select name="daySelect" 
-                id="daySelect"
-                class="text-center mx-2"
-                @change="filter(day,genre)"
-                v-model="day">
-        
-          <option v-for="day in days"
-                  :value="day">
-            {{ day }}
-          </option>
-        </select>
+    <!-- dates -->
+    <div class="row justify-content-center">
+      
+      <!-- title -->
+      <div class="col-12">
+        <h1 class="text-center text-white mb-5">
+          Vetítési szűrő
+        </h1>
+      
+        <hr class="text-white my-3 mt-4">
       </div>
+  
+      <!-- selection -->
+      <div class="row text-center fs-2">
 
-      <!-- genre -->
-      <div class="w-50">
-        <label for="genreSelect" 
+        <!-- day -->
+        <div class="col-12 col-md-6 my-2">
+          <label for="daySelect" 
                class="text-light">
-          Műfaj:
-        </label>
+            Nap:
+          </label>
 
-        <select name="genreSelect" 
-                id="genreSelect"
-                class="text-center mx-2"
-                @change="filter(day,genre)"
-                v-model="genre">
-        
-            <option value="all">Összes Műfaj</option>
-            <option v-for="genre in genres"
-                    :key="genre"
-                    :value="genre">
-                {{ genre }}
+          <select name="daySelect" 
+                  id="daySelect"
+                  class="text-center mx-2"
+                  @change="filter(day,genre)"
+                  v-model="day">
+
+            <option v-for="day in days"
+                    :value="day">
+              {{ day }}
             </option>
-        </select>
+          </select>
+        </div>
+
+        <!-- genre -->
+        <div class="col-12 col-md-6 my-2">
+          <label for="genreSelect" 
+                 class="text-light">
+            Műfaj:
+          </label>
+
+          <select name="genreSelect" 
+                  id="genreSelect"
+                  class="text-center mx-2"
+                  @change="filter(day,genre)"
+                  v-model="genre">
+
+              <option value="all">Összes Műfaj</option>
+              <option v-for="genre in genres"
+                      :key="genre"
+                      :value="genre">
+                  {{ genre }}
+              </option>
+          </select>
+        </div>
+      </div>
+    
+      <div class="col-12">
+        <hr class="text-white my-3">
       </div>
     </div>
-  
-    <div class="col-12">
-      <hr class="text-white my-3">
-    </div>
-  </div>
 
-    <!-- movie div -->
-    <div class="row g-3">
-      <div v-for="movie in movies" :key="movie.id"
-           class="p-2 h-100 px-2 py-2 col-12 col-md-6 col-lg-4 col-xl-3">
-
-        <!-- movie -->
-        <div class="movie-card d-flex flex-row flex-md-column border
-                    align-items-md-center border-black
-                    text-md-center bg-primary-subtle">
+    <!-- movies -->
+    <div class="container my-2">
+      <div class="row justify-content-center">
+        <div class="col-11 col-sm-8 col-md-10 col-lg-10 
+                    col-xl-10 col-xxl-12">
+          <div class="movie-card d-flex flex-column flex-md-row 
+                      align-items-center p-2 shadow-lg my-5"
+               v-for="movie in movies">
+        
+            <!-- Poster -->
+            <div class="poster-wrapper">
+              <img :src="`./src/assets/media/images/movie_posters/${movie.poster}`"
+                   class="movie-image">
+            </div>
           
-          <!-- movie image -->
-          <img :src="`./src/assets/media/images/movie_posters/${movie.poster}`" 
-               class="movie-image">
-
-          <!-- movie content -->
-          <div class="mt-2 mx-3">
-            <h3 class="fs-3 my-1 fw-bold">{{ movie.movie_title }}</h3>
-            <p class="fs-4 mb-1 text-muted">{{ movie.genre }} | {{ movie.runtime }} perc</p>
-            <p class="fs-4 mb-1 text-muted">{{ movie.start }} - {{ movie.end }}</p>
-            <p class="fs-4 mb-2 text-muted">Terem: {{ movie.room_id}}</p>
-          
-            <!-- movie time -->
-            <div class="movie-times d-flex flex-wrap gap-1 
-                        justify-content-start 
-                        justify-content-md-center fs-5">
-
-              <!-- movie time display -->
-              <span class="badge bg-secondary mb-1"> 
-                {{ movie.showing_in }}
-              </span>
-              <span class="badge bg-warning mb-1"> {{ movie.language }}</span>
+            <!-- Content -->
+            <div class="movie-content ms-md-4 mt-4 mt-md-0 text-center text-md-start">
+            
+              <h1 class="fw-bold mb-3">
+                {{ movie.movie_title }}
+              </h1>
+            
+              <p class="fs-3 text-muted mb-2">
+                {{ movie.genre }} | {{ movie.runtime }} perc
+              </p>
+            
+              <p class="fs-3 text-muted mb-2">
+                {{ movie.start }} - {{ movie.end }}
+              </p>
+            
+              <p class="fs-3 text-muted mb-3">
+                Terem: {{ movie.room_id }}
+              </p>
+            
+              <div class="d-flex flex-wrap gap-2 fs-4 
+                          justify-content-center 
+                          justify-content-md-start">
+            
+                <span class="badge bg-success">
+                  {{ movie.showing_in }}
+                </span>
+              
+                <span class="badge bg-info">
+                  {{ movie.language }}
+                </span>
+              </div>
             </div>
           </div>
         </div>
@@ -166,7 +183,7 @@
    * considering the runtime and timing of the movies
    */
   function createScreeningDays() {
-    
+    console.log(allMovies.value.length);
 
 
   }
@@ -188,6 +205,7 @@
       genres.value.push(genresData.data[i].genre);
     }
 
+    createScreeningDays();
     console.log(genres.value);
   })
 </script>
