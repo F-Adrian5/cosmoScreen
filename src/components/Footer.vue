eeda06069669cdd783ca2d251b38f3b55b5ccd9d
F-Adrian5 <fodor.adrian-2021@keri.mako.hu>
2026-03-12 13:19:41 +0100

    Footer design fejlesztése

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,27 +1,55 @@
 <template>
   <div class="container-fluid bg-body-tertiary mt-auto footerStyle">
-    <div class="row justify-content-center align-items-center 
-                py-3 bg-dark text-light">
+    <div class="row justify-content-center align-items-center py-3 bg-dark text-light">
       
       <!-- brand -->
-      <div class="col footerLogo text-center mb-3 fs-3">
+      <div class="col-lg-3 col-md-6 col-12 footer-col footerLogo text-center mb-3 fs-3">
         CosmoScreen
+        <br>
+        <span class="fs-6">
+          „Ahol minden képkocka egy új világ!”
+        </span>
+      </div>
+
+      <!-- location -->
+      <div class="col-lg-3 col-md-6 col-12 footer-col text-center fw-bold mb-3 fs-5">
+        <i class="fa-solid fa-map-location-dot"></i>
+        Látogass meg minket
+        <div class="fw-light fs-5 footerText">
+          <div>
+            6900 Makó, Fő utca 12.
+          </div>
+          <div>
+            <a href="https://maps.google.com/?q=Makó+Fő+utca+12" 
+               target="_blank" 
+               class="link_color">
+              Megnyitás térképen
+            </a>
+          </div>
+        </div>
       </div>
       
       <!-- contacts -->
-      <div class="col text-center fw-bold mb-3 fs-4">
+      <div class="col-lg-3 col-md-6 col-12 footer-col text-center fw-bold mb-3 fs-5">
+        <i class="fa-solid fa-address-book"></i>
         {{ $t('footer.contacts') }}
         <div class="fw-light fs-5 footerText">
-          <div>CosmoScreen@gmail.com</div>
-          <div>Tel.: +36303456789</div>
+          <div>
+            <i class="fa-solid fa-envelope"></i>
+            CosmoScreen@gmail.com
+          </div>
+          <div>
+            <i class="fa-solid fa-phone"></i>
+            Tel.: +36303456789
+          </div>
         </div>
       </div>
       
       <!-- informations -->
-      <div class="col fw-bold text-center mb-3 fs-4">
+      <div class="col-lg-3 col-md-6 col-12 footer-col fw-bold text-center mb-3 fs-5">
+        <i class="fa-solid fa-circle-info"></i>
         {{ $t('footer.informations') }}
 
-        <!-- rules -->
         <div class="fw-light fs-5 footerText">
           <div>
             <RouterLink class="link_color" to="/rules">
@@ -29,21 +57,21 @@
             </RouterLink>
           </div>
           
-          <!-- data fishing -->
           <div>
             <RouterLink class="link_color" to="/dataFishing">
               {{ $t('footer.dataFishing') }}
             </RouterLink>
           </div>
-        
         </div>
       </div>
+
       <hr>
       
       <!-- watermark -->
-      <p class="fs-xs text-start mb-0 text-center fw-bold fs-5">
-       {{ $t('footer.copyright') }} {{ new Date().getFullYear() }} &copy;
+      <p class="fs-xs mb-0 text-center fw-bold fs-5">
+        {{ $t('footer.copyright') }} {{ new Date().getFullYear() }} &copy;
       </p>
+
     </div>
   </div>
 </template>
